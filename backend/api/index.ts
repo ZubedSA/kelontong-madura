42ded0707b2faf766d166c775654a8924e475995
zubed <achzubaidi07@gmail.com>
2026-07-22 17:37:24 +0700

    Update Backend agar bisa gratis di Vercel

---
+++ backend/api/index.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from '../src/app.module';
+import * as express from 'express';
+
+let cachedServer: express.Express | undefined;
+
+async function bootstrap() {
+  if (!cachedServer) {
+    const expressApp = express();
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp)
+    );
+    app.enableCors();
+    await app.init();
+    cachedServer = expressApp;
+  }
+  return cachedServer;
+}
+
+export default async function (req: any, res: any) {
+  const server = await bootstrap();
+  server(req, res);
+}
